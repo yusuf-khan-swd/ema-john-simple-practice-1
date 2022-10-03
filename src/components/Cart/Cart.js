a492f714f1ae0fc061a7e01a04ec5801ecd5e6d5
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-03 19:13:01 +0600

    a raft calculation done on added cart item

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
 const Cart = ({ cart }) => {
-  console.log(cart)
+
+  let totalPrice = 0;
+  let shippingCharge = 0;
+  for (let product of cart) {
+    totalPrice += product.price;
+    shippingCharge += product.shipping;
+  }
+
+  const tax = totalPrice * 0.1;
+  const grandTotal = totalPrice + shippingCharge + tax;
+
   return (
     <div className='sticky top-0'>
-      <h3 className='text-xl text-center mt-7'>Order Summery</h3>
+      <h3 className='text-xl text-center mt-7 mb-6 font-medium'>Order Summery</h3>
       <div className='p-6'>
-        <p>Selected Items: {cart.length} </p>
+        <p className='mb-6'>Selected Items: {cart.length} </p>
+        <p className='mb-6'>Total Price: ${totalPrice} </p>
+        <p className='mb-6'>Total Shipping Charge: ${shippingCharge} </p>
+        <p className='mb-6'>Tax: ${tax.toFixed(2)} </p>
+        <p className='mb-6 text-xl font-medium'>Grand Total: ${grandTotal.toFixed(2)} </p>
       </div>
     </div>
   );
